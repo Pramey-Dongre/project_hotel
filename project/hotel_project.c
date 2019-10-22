59fb036783d69a51ead26e9212c1687fe8e79424
Pramey-Dongre <55648461+Pramey-Dongre@users.noreply.github.com>
2019-10-22 23:37:48 +0530

    Update hotel_project.c

--- project/hotel_project.c
+++ project/hotel_project.c
@@ -2,6 +2,36 @@
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
+#define MAX 50
+#define SIZE 30
+typedef struct
+{
+    char s[MAX][SIZE];
+    int f,r;
+}queue;
+void insert(queue *t ,char ele[])
+{
+    if(t->r == MAX -1)
+    {
+        printf("\nQueue full");
+        return;
+    }
+    t->r++;
+    strcpy(t->s[t->r],ele);
+}
+void xdelete(queue *t)
+{
+    char z[30];
+    strcpy(z,t->s[t->f]);
+    t->f++;
+}
+void display(queue *t)
+{
+    int i;
+    for(i=t->f;i<=t->r;i++)
+        printf("%s   ",t->s[i]);
+    printf("\n");
+}
 int main()
 {
     FILE *fp;
@@ -11,14 +41,17 @@ int main()
         printf("\nwrong");
         exit(0);
     }
+    queue x;
+    x.f=0;
+    x.r = -1;
     int ch,pr;
     float tot=0,gst,price;
     time_t t;
     char b[100],s;
-    fputs("\n\t\t\tWelcome to XXX Hotel!!!!!!\n",fp);
+    fputs("\n\t\t\tWelcome to PethPooja Hotel!!!!!!\n",fp);
     while(1)
     {
-    printf("\n\t\t\tWelcome to XXX Hotel!!!!!!");
+    printf("\n\t\t\tWelcome to PethPooja Hotel!!!!!!");
     printf("\nwe serve the following delicacies.......");
     printf("\n\t***Breakfast***");
     printf("\n\t1.Poha -- Rs.30");
@@ -28,9 +61,11 @@ int main()
         printf("\n\t***Lunch***");
     printf("\n\t5.Rice Plate -- Rs.90");
     printf("\n\t6.Noodles -- Rs.120");
+    printf("\n\t***Dinner***");
     printf("\n\t7.Paneer tikka -- Rs.150");
     printf("\n\t8.Chicken Tandoori -- Rs.220");
-    printf("\nEnter choice:--");
+    printf("\n\t9.Rogan Josh -- Rs.390");
+    printf("\n 0. to exit\nEnter choice:--");
     scanf("%d",&ch);
     if(ch == 0)
         break;
@@ -38,30 +73,43 @@ int main()
     {
         case 1:fputs("Poha..--Rs.30\n",fp);
                    tot=tot+30;
+                   insert(&x,"Poha");
                    break;
         case 2:fputs("Upma..--Rs.45\n",fp);
                     tot= tot + 45;
+                    insert(&x,"Upma");
                     break;
         case 3:fputs("Idli..--Rs.50\n",fp);
                     tot = tot + 50;
+                    insert(&x,"Idli");
                     break;
         case 4:fputs("Dosa..--Rs.70\n",fp);
                    tot = tot + 70;
+                   insert(&x,"Dosa");
                    break;
         case 5:fputs("Rice Plate..--Rs.90\n",fp);
                     tot = tot + 90;
+                    insert(&x,"Rice Plate");
                     break;
         case 6:fputs("Noodles..--Rs.120\n",fp);
                     tot = tot + 120;
+                    insert(&x,"Noodles");
                     break;
         case 7:fputs("Paneer Tikka..--Rs.150\n",fp);
                     tot = tot + 150;
+                    insert(&x,"Paneer Tikka");
                     break;
         case 8:fputs("Chicken Tandoori ..--Rs.220\n",fp);
                     tot = tot + 220;
+                    insert(&x,"Chicken Tandoori");
+                    break;
+        case 9:fputs("Rogan Josh...--Rs.390\n",fp);
+                    tot = tot +390;
+                    insert(&x,"Rogan Josh");
                     break;
     }
     }
+    display(&x);
     gst=9*tot/100;
     price=tot+gst;
     fprintf(fp,"Total = %f\nGST(9per.) = %f\nGrand Total=%f\n",tot,gst,price);
@@ -70,6 +118,11 @@ int main()
     fputs(__DATE__,fp);
     fclose(fp);
     fp=fopen("BILL.TXT","r");
+    if(fp==NULL)
+	{
+		printf("\nCAN NOT OPEN FILE");
+		exit(0);
+	}
     do
     {
 		s=getc(fp);
